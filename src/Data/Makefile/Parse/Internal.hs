82d15b10f7347b8f52af4d5384064872339e2d64
Eric Wolf <ericwolf42@gmail.com>
2017-11-20 12:46:03 +0100

    first test case, some names to catch errors

--- src/Data/Makefile/Parse/Internal.hs
+++ src/Data/Makefile/Parse/Internal.hs
@@ -4,7 +4,7 @@ module Data.Makefile.Parse.Internal where
 import Data.Makefile
 
 import qualified Data.Attoparsec.Text as P
-import Data.Attoparsec.Text (Parser)
+import Data.Attoparsec.Text (Parser, (<?>))
 
 import qualified Data.Text as T
 import Data.Text (Text)
@@ -37,10 +37,10 @@ recipeLine2 = do
   return $ LT.toStrict $ LTB.toLazyText b
 
 lineContinuation :: Parser Text
-lineContinuation = P.string "\\\n"
+lineContinuation = P.string "\\\n" <?> "line continuation"
 
 recipeLineContinuation :: Parser Text
-recipeLineContinuation = P.string "\\\n\t" >> return "\\\n"
+recipeLineContinuation = P.string "\\\n\t" >> return "\\\n" <?> "recipe line continuation"
 
 recipeLine :: Parser RecipeLine
 recipeLine = do
@@ -53,54 +53,54 @@ isLineSpace :: Char -> Bool
 isLineSpace c = c /= '\n' && isSpace c
 
 lineSpaceCont :: (Parser Text -> Parser [Text]) -> Parser Text
-lineSpaceCont f = P.takeWhile isLineSpace >> f (lineContinuation >> P.takeWhile isLineSpace) >> return " "
+lineSpaceCont f = (P.takeWhile isLineSpace >> f (lineContinuation >> P.takeWhile isLineSpace) >> return " ") <?> "line space or line continuation"
 
 lineSpace :: Parser Text
 lineSpace = lineSpaceCont P.many'
 
 
 assignOp :: Parser AssignOp
-assignOp = (P.string "::=" >> return PosixSimple)
+assignOp = ((P.string "::=" >> return PosixSimple)
        <|> (P.string ":=" >> return Simple)
        <|> (P.string "?=" >> return Conditional)
        <|> (P.string "!=" >> return Shell)
        <|> (P.string "+=" >> return Append)
-       <|> (P.string "=" >> return Recursive)
+       <|> (P.string "=" >> return Recursive)) <?> "assign operation"
 
 variableNameChunk :: Parser Text
 variableNameChunk = P.takeWhile1 (\c -> c /= '\\' && c /= '\n'
                                   && not (isSpace c)
                                   && c /= '#'  && c /= ':' && c /= '='
-                                  && c /= '!' && c /= '?')
+                                  && c /= '!' && c /= '?') <?> "variable name chunk"
 
 manyTill :: Parser a -> Parser b -> Parser ([a], b)
-manyTill elemP end = scan []
+manyTill elemP end = scan [] <?> "many till"
   where
     scan xs = do { endVal <- end; return (reverse xs, endVal) ; } <|> do { nextVal <- elemP; scan (nextVal : xs) ; }
 
 -- no :, =, # or whitespace
 variableName :: Parser (Text, AssignOp)
-variableName = do
+variableName = (do
   _ <- lineSpace
   (chunks, assign) <- manyTill (P.string "!" <|> P.string "?" <|> P.string "\\" <|> variableNameChunk) (lineSpace *> assignOp)
-  return (T.concat chunks, assign)
+  return (T.concat chunks, assign)) <?> "variable name and assignment"
 
 commentOrMakeTextChunk :: String -> Bool -> Parser Text
 commentOrMakeTextChunk escapedOk keepQuotedSpaceQuoted = let escapes = P.choice (map (\c -> P.char '\\' >> P.char c >> return (T.singleton c)) escapedOk) in
-  lineSpaceCont P.many1' -- condense space around line continuation
+  (lineSpaceCont P.many1' -- condense space around line continuation
   <|> P.takeWhile1 isLineSpace -- take space unchanged otherwhise
   <|> escapes -- one of the chars, which are ok if quoted
   <|> if keepQuotedSpaceQuoted
       then fmap (T.cons '\\' . T.singleton) (P.char '\\' >> P.satisfy isSpace) -- quoted space needs to preserve the quote -- testcase "\\  \\\n " must become "\\  " and not "\\ "
       else empty
   <|> P.string "\\" -- a quote itself is ok if it doesn't mask some magic char.
-  <|> P.takeWhile1 (\c -> notElem c escapedOk && c /= '\\' && c /= '\n' && not (isSpace c))
+  <|> P.takeWhile1 (\c -> notElem c escapedOk && c /= '\\' && c /= '\n' && not (isSpace c))) <?> "basic make text chunk"
 
 comment :: Parser Text
-comment = do
+comment = (do
   _ <- P.char '#'
   rest <-  P.many' $ commentOrMakeTextChunk "" False
-  return $ T.concat rest
+  return $ T.concat rest) <?> "comment"
 
 standardMakeTextChunk :: String -> Parser Text
 standardMakeTextChunk escapedOk = commentOrMakeTextChunk escapedOk True
@@ -112,7 +112,7 @@ variableAssignment = do
   rest <- P.many' $ standardMakeTextChunk "#"
   let vartext = T.concat rest
   commentT <- P.option "" comment
-  _ <- P.char '\n'
+  _ <- P.char '\n' <?> "variable assignment newline"
   return $ VariableAssignment (VariableName name) assign (VariableValue vartext) (Comment commentT)
 
 simpleRule :: Parser Entry
---
+++ src/Test.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Test.Hspec
+import Data.Attoparsec.Text
+import Data.Makefile
+import Data.Makefile.Parse.Internal
+
+main :: IO()
+main = hspec $ do
+  describe "basic make text parsing" $
+    it "parses \"abc\" as a standard make text" $
+      parseOnly (standardMakeTextChunk "#") "abc" `shouldBe` (Right "abc")
+  describe "Basic Variable Assignment" $
+    it "parses a = b\\n" $
+      parseOnly variableAssignment "a = b\n" `shouldBe` Right (VariableAssignment (VariableName "a") Simple (VariableValue "b") (Comment ""))
