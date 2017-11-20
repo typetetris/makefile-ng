929d72c4ad2d0111aae5ef024ef6be4b45546ad5
Eric Wolf <ericwolf42@gmail.com>
2017-11-20 22:19:04 +0100

    a few more test cases

--- src/Test.hs
+++ src/Test.hs
@@ -6,11 +6,25 @@ import Data.Attoparsec.Text
 import Data.Makefile
 import Data.Makefile.Parse.Internal
 
+simpleAEqualb :: Either a Entry
+simpleAEqualb = Right (VariableAssignment (VariableName "a") Recursive (VariableValue "b") (Comment ""))
+
+
 main :: IO()
 main = hspec $ do
   describe "basic make text parsing" $
     it "parses \"abc\" as a standard make text" $
       parseOnly (standardMakeTextChunk "#") "abc" `shouldBe` (Right "abc")
-  describe "Basic Variable Assignment" $
+  describe "Basic Variable Assignment" $ do
     it "parses a = b\\n" $
-      parseOnly variableAssignment "a = b\n" `shouldBe` Right (VariableAssignment (VariableName "a") Recursive (VariableValue "b") (Comment ""))
+      parseOnly variableAssignment
+      "a = b\n" `shouldBe` simpleAEqualb
+    it "parses a \\\n    = b\\n" $
+      parseOnly variableAssignment
+      "a \\\n   = b\n" `shouldBe` simpleAEqualb
+    it "parses a \\\n  = \\\nb\\n" $
+      parseOnly variableAssignment
+      "a \\\n   = \\\nb\n" `shouldBe` simpleAEqualb
+    it "parses a \\\n  = \\\n\tb\\n" $
+      parseOnly variableAssignment
+      "a \\\n   = \\\n\tb\n" `shouldBe` simpleAEqualb
